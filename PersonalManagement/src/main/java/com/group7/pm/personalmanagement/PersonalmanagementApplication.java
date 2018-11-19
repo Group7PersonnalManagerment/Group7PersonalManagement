028aa6819dd409eb67ae5de68a5b0238537541f6
严浩天 <41533033+ziqingkeshi@users.noreply.github.com>
2018-11-19 20:05:31 +0800

    Add files via upload

---
+++ PersonalManagement/src/main/java/com/group7/pm/personalmanagement/PersonalmanagementApplication.java
@@ -0,0 +1,12 @@
+package com.group7.pm.personalmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalmanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalmanagementApplication.class, args);
+    }
+}
---
+++ PersonalManagement/src/test/java/com/group7/pm/personalmanagement/PersonalmanagementApplicationTests.java
@@ -0,0 +1,16 @@
+package com.group7.pm.personalmanagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PersonalmanagementApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
